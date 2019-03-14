eb43db64d97084a5f466d12e630be18551b9471c
Julio Sanchez <jsanchez191980802@gmail.com>
2019-03-14 01:22:45 -0500

    added delete icon

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,6 +24,10 @@ class Contact extends Component {
             className="fas fa-sort-down"
             style={{ cursor: "pointer" }}
           />{" "}
+          <i
+            className="fas fa-times"
+            style={{ cursor: "pointer", float: "right", color: "red" }}
+          />
         </h4>
         {showContactInfo ? (
           <ul className="list-group">
