6d81930fa6917fb4f20c74564b2e174b0dd26f92
Julio Sanchez <jsanchez191980802@gmail.com>
2019-03-14 01:00:46 -0500

    add ternary to show info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,22 +2,31 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
-  state = {};
+  state = {
+    showContactInfo: false
+  };
 
-  onShowClick = e => {};
+  onShowClick = e => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
+  };
 
   render() {
     const { contact } = this.props;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
         <h4>
           {contact.name}{" "}
           <i onClick={this.onShowClick} className="fas fa-sort-down" />{" "}
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">email:{contact.email}</li>
-          <li className="list-group-item">phone: {contact.phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">email:{contact.email}</li>
+            <li className="list-group-item">phone: {contact.phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
